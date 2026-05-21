97d2edc58e7d1c47806f10802a40d56b70fa4890
Dante <j2005gabriel@gmail.com>
2026-05-20 21:35:33 -0300

    adicionei mensagens para a descrição do programa

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 
 #define T 3 // -> Grau minimo/ O máximo seria: 2T -1 Chaves
 
+//CONFERÊNCIA:
+//a raiz é uma folha ou possui no mínimo dois filhos;
+//cada nó interno (exceto a raiz) possui no mínimo d + 1 filhos;
+//cada nó possui no máximo 2d+1 filhos;
+//todas as folhas estão no mesmo nível.
+
 typedef struct no {
     int chaves[2*T - 1]; //Array das chaves
     struct no *filho[2*T];
@@ -30,4 +36,5 @@ void insert();
 void ordem();
 
 int main(){
+    // colocar 1-Busca 2-Inserir 3-Finalizar
 }
